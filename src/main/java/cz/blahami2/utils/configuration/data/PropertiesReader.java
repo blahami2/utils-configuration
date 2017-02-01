86f23eb7f6010669525dc47eda6e74ce9776cb08
Michael Bláha <blahami2@gmail.com>
2017-02-01 10:33:59 +0100

    Replaced checked exceptions with unchecked

--- src/main/java/cz/blahami2/utils/configuration/data/PropertiesReader.java
+++ src/main/java/cz/blahami2/utils/configuration/data/PropertiesReader.java
@@ -1,13 +1,12 @@
 package cz.blahami2.utils.configuration.data;
 
+import cz.blahami2.utils.configuration.data.readers.PropertiesConfigurationReader;
+
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Michael Blaha {@literal <blahami2@gmail.com>}
@@ -15,19 +14,28 @@ import java.util.stream.Collectors;
 public class PropertiesReader {
     private Map<String, ConfigurationReader> formatReaderMap = new HashMap<>();
 
+    public PropertiesReader() {
+        this.formatReaderMap = Stream.of( new PropertiesConfigurationReader() ).collect( Collectors.toMap( reader -> reader.getExtension(), reader -> reader ) );
+    }
+
     public PropertiesReader( Collection<ConfigurationReader> readers ) {
         this.formatReaderMap = readers.stream().collect( Collectors.toMap( reader -> reader.getExtension(), reader -> reader ) );
     }
 
-    public Properties read( String path ) throws IOException {
+
+    public Properties read( String path ) {
         File file = new File( path );
         if ( !file.exists() ) {
-            throw new FileNotFoundException( "File does not exist: " + path );
+            throw new RuntimeException( "File does not exist: " + path );
         }
         String extension = path.substring( path.lastIndexOf( '.' ) + 1 );
-        if(!formatReaderMap.containsKey( extension )){
+        if ( !formatReaderMap.containsKey( extension ) ) {
             throw new IllegalArgumentException( "Unknown extension: " + extension );
         }
-        return formatReaderMap.get( extension ).loadProperties(file);
+        try {
+            return formatReaderMap.get( extension ).loadProperties( file );
+        } catch ( IOException e ) {
+            throw new RuntimeException( "Exception thrown when loading properties from a file.", e );
+        }
     }
 }
